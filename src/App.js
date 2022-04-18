81c76534c8b1300c39422532ad0cf1f8263f9f0e
Amethyst <2810990752@qq.com>
2022-04-18 15:00:53 +0800

    add React.lazy

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import React,{useEffect,useCallback} from 'react';
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
-import Login from './pages/login';
-import Home from './pages/home';
-import Celebrities from './pages/celebrities';
-import Goals from "./pages/goals";
-import Financial from "./pages/finacial";
-import SummaryDetail from "./pages/summary_detail";
-import RewardPunishment from './pages/punishments';
-import HistoryOfLc from "./pages/history_lc";
-import Manage from './pages/manage';
-import Space from './pages/space';
-import Album from './pages/album';
-import UpdateUserInfo from './pages/space/components/userinfo_update';
-import SummaryAddOrUpdate from './pages/summary-add';
-import AlbumDetail from './pages/album-detail';
-import TeamMembers from "./pages/members";
-import HistoryNews from './pages/history_news';
-import IntroductionDoc from "./pages/docs";
-import Test from './pages/test';
+import React, { useEffect, useCallback, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'animate.css';
 import storageUtils from './utils/storageUtils';
-import {reqQueryAvatar} from './api';
+import { reqQueryAvatar } from './api';
+const Login = React.lazy(() => import('./pages/login'));
+const Home=React.lazy(()=>import('./pages/home'));
+const Celebrities=React.lazy(()=>import('./pages/celebrities'));
+const Goals=React.lazy(()=>import('./pages/goals'));
+const Financial=React.lazy(()=>import('./pages/finacial'));
+const SummaryDetail=React.lazy(()=>import('./pages/summary_detail'));
+const RewardPunishment=React.lazy(()=>import('./pages/reward-punish'));
+const HistoryOfLc=React.lazy(()=>import('./pages/history_lc'));
+const Manage=React.lazy(()=>import('./pages/manage'));
+const Space=React.lazy(()=>import('./pages/space'));
+const Album=React.lazy(()=>import('./pages/album'));
+const UpdateUserInfo=React.lazy(()=>import('./pages/space/components/userinfo_update'));
+const SummaryAddOrUpdate=React.lazy(()=>import('./pages/summary-add'));
+const AlbumDetail=React.lazy(()=>import('./pages/album-detail'));
+const TeamMembers=React.lazy(()=>import('./pages/members'));
+const HistoryNews=React.lazy(()=>import('./pages/history_news'));
+const IntroductionDoc=React.lazy(()=>import('./pages/docs'));
+
 
 function App() {
-  const {userid,role}=storageUtils.getUser();
+  const { userid, role } = storageUtils.getUser();
 
-  const isTokenValid=()=>{
-    const startTime=storageUtils.getStartTime();
-    const now=Date.now();
-    if((now-startTime)/(1000*60*60*24)>=3) return false;
+  const isTokenValid = () => {
+    const startTime = storageUtils.getStartTime();
+    const now = Date.now();
+    if ((now - startTime) / (1000 * 60 * 60 * 24) >= 3) return false;
     return true;
   }
 
   //若token过期
-  if(!isTokenValid()){
+  if (!isTokenValid()) {
     storageUtils.removeUser();
     //跳转到login
   }
@@ -42,18 +42,18 @@ function App() {
   const getAvatarRole = useCallback(async () => {
     const res = await reqQueryAvatar(userid);
     if (res.code === 1) {
-        if(res.data){
-            const user_role=res.data.role;
-            //判断角色是否更新
-            if(role!==user_role){
-                //通过，修改缓存
-                const local_user=storageUtils.getUser();
-                local_user.role=user_role;
-                storageUtils.addUser(local_user);
-            }
+      if (res.data) {
+        const user_role = res.data.role;
+        //判断角色是否更新
+        if (role !== user_role) {
+          //通过，修改缓存
+          const local_user = storageUtils.getUser();
+          local_user.role = user_role;
+          storageUtils.addUser(local_user);
         }
       }
-  }, [userid,role]);
+    }
+  }, [userid, role]);
 
   useEffect(() => {
     getAvatarRole();
@@ -61,26 +61,27 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path='/login' exact component={Login}></Route>
-        <Route path='/celebrities' exact component={Celebrities}></Route>
-        <Route path='/goals' exact component={Goals}></Route>
-        <Route path='/financial' component={Financial}></Route>
-        <Route path='/summary_detail' component={SummaryDetail}></Route>
-        <Route path='/history' component={HistoryOfLc}></Route>
-        <Route path='/reward_punishment' component={RewardPunishment}></Route>
-        <Route path='/manage' component={Manage}></Route>
-        <Route path='/space/updateuserinfo'  component={UpdateUserInfo}></Route>
-        <Route path='/space/summaryadd' component={SummaryAddOrUpdate}></Route>
-        <Route path='/space' component={Space}></Route>    
-        <Route path='/album/detail'component={AlbumDetail} />
-        <Route path='/album' component={Album}></Route>
-        <Route path='/members' component={TeamMembers}></Route>
-        <Route path='/history_news' component={HistoryNews}></Route>
-        <Route path='/docs' component={IntroductionDoc}></Route>
-        <Route path='/test' component={Test}></Route>
-        <Route path='/' component={Home}></Route>
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path='/login' exact component={Login}></Route>
+          <Route path='/celebrities' exact component={Celebrities}></Route>
+          <Route path='/goals' exact component={Goals}></Route>
+          <Route path='/financial' component={Financial}></Route>
+          <Route path='/summary_detail' component={SummaryDetail}></Route>
+          <Route path='/history' component={HistoryOfLc}></Route>
+          <Route path='/reward_punishment' component={RewardPunishment}></Route>
+          <Route path='/manage' component={Manage}></Route>
+          <Route path='/space/updateuserinfo' component={UpdateUserInfo}></Route>
+          <Route path='/space/summaryadd' component={SummaryAddOrUpdate}></Route>
+          <Route path='/space' component={Space}></Route>
+          <Route path='/album/detail' component={AlbumDetail} />
+          <Route path='/album' component={Album}></Route>
+          <Route path='/members' component={TeamMembers}></Route>
+          <Route path='/history_news' component={HistoryNews}></Route>
+          <Route path='/docs' component={IntroductionDoc}></Route>
+          <Route path='/' component={Home}></Route>
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   );
 }
--- src/pages/test/index.js
+++
@@ -1,3 +0,0 @@
-export default function Test(){
-    return <h3>测试自动化部署</h3>
-}
